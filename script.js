af4836e6cd1546377984e805596235fd7dffbf61
Filipe Viana Lacerda <filipe.viana@ufv.br>
2021-08-03 21:49:23 -0300

    is_palindrome function

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function change_response(element, message, color){
     element.style.display = 'block';
     element.style.backgroundColor = color;
 }
+
+function is_palindrome(){
+    const word = document.getElementById('word').value;
+    const word_reverse = word.split("").reverse().join("");
+    var response_block = document.getElementById('response');
+    if(word === word_reverse)
+        change_response(response_block,"Yes! This word is a palindrome",'green');
+    else
+        change_response(response_block,"Uou! This word is not a palindrome",'red');
+}
